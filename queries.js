e6be106aa511adb2e1f22a6a4d30a9df330a1204
Trevor French <trevorf96@gmail.com>
2021-01-09 22:58:27 -0700

    updating navBar

--- queries.js
+++ queries.js
@@ -84,7 +84,7 @@ const selectOpen = function(req, res) {
 		  throw error;
 	  }
 	  var tableText = '<table><tr><th>TITLE</th><th>PROJECT ID</th><th>STATUS</th><th>RESPONSIBLE</th><th>DUE DATE</th><th>DESCRIPTION</th></tr>';
-	  results.rows.forEach(element => tableText += '<tr><td><form id="projectform" action="/openProject" method="post"><input type=text name=ticketID value=' + element.project_id + ' id=' + element.project_id + ' hidden>' + '<a href="javascript: submitproject()">' + element.title + '</a></form></td><td>' + element.project_id + '</td><td>' + element.status + '</td><td>' + element.responsible + '</td><td>' + element.duedate + '</td><td>' + element.description + '</td></tr>');
+	  results.rows.forEach(element => tableText += '<tr><td><form id="projectform" action="/openProject" method="post"><input type=text name=ticketID value=' + element.project_id + ' id=' + element.project_id + ' hidden>' + '<input type="submit" value="' + element.title + '"></form></td><td>' + element.project_id + '</td><td>' + element.status + '</td><td>' + element.responsible + '</td><td>' + element.duedate + '</td><td>' + element.description + '</td></tr>');
 	tableText += '</table>';
 	res.render("dashboard.ejs", {statusMessage: tableText})
 })
