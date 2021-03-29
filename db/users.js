29121ce70817ac05ead197c53641307bb98b6498
Trevor French <trevorf96@gmail.com>
2021-03-29 14:14:12 -0600

    search bar

--- db/users.js
+++ db/users.js
@@ -64,7 +64,7 @@ exports.createProject2 = function(req, res) {
                                ul.style.display = 'block';\
                                for (i = 0; i < li.length; i++) {\
                                    a = li[i].getElementsByTagName('a')[0];\
-                                   if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {\
+                                   if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {\
                                      li[i].style.display = '';\
                                    } else {\
                                      li[i].style.display = 'none';\
