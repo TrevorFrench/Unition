3f4f41795b62b2ee1d543b4f652fb4b940dfeeb0
Trevor French <trevorf96@gmail.com>
2021-05-06 09:18:25 -0600

    documenting error

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED='0' // Also did this: npm config set st
    - get rid of open team input on sign up form
    - make documentation text color variable
    - get rid of responsible on project creation form (individual)
+   -internal server error when you hit  home after cookie times out
 */
 
 //-----------------------------------------------------------------
