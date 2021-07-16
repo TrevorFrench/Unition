60aa17032dc31362ce681978b7b3ba500a255f2a
Trevor French <trevorf96@gmail.com>
2021-07-16 11:13:05 -0600

    landing page

--- server.js
+++ server.js
@@ -104,6 +104,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED='0' // Also did this: npm config set st
    - Create sub tasks (maybe as a type of comment)
    - comments start offset to the right
    - Success form
+   - REDIRECT AFTER REATING A TEAM PROJECT
 */
 
 //------------------------------------------------------------------------------
