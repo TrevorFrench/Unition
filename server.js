6cc66612211fdb5e01ac62f6375d067e5bc7d443
Trevor French <42419234+TrevorFrench@users.noreply.github.com>
2022-07-14 13:38:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -113,6 +113,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED='0' // Also did this: npm config set st
    - Design chart divs (border radius)
    - Montserrat vs. Roboto?
    - Maybe make teams look more like powerbi browser version
+   - github env variables for db
 */
 
 //------------------------------------------------------------------------------
